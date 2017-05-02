9e858642cf19c88138df26af93418425f4dda5d4
zhanglihao <zhanglihao@qjdchina.com>
2017-05-02 20:01:50 +0800

    修改内容

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,8 @@ Page({
             lastPos: {
                 x: 0,
                 y: 0
-            }
+            },
+            isClear:false
         }
     },
     //事件处理函数
@@ -23,9 +24,6 @@ Page({
         wx.navigateTo({
             url: '../other/other'
         })
-    },
-    canvasIdErrorCallback: function(e) {
-        console.error(e.detail.errMsg)
     },
     onLoad: function() {
         // 获取系统信息
@@ -56,7 +54,13 @@ Page({
 
         this.context = wx.createCanvasContext('canvas');
 
+        let isClear=this.data.canvasInfor.isClear;
+        
+        this.context.setStrokeStyle('rgba(255,255,255,0)')
+    
         this.context.setStrokeStyle('rgba(0,0,0,1)')
+        
+        
         this.context.setLineWidth(6)
          // 让线条圆润 
         this.context.setLineCap('round')
@@ -66,10 +70,11 @@ Page({
     touchMoveFun(e) {
         let curPos=e.touches[0],
             lastPos=this.data.canvasInfor.lastPos;
-        
+        // this.context.save()
         this.context.moveTo(lastPos.x,lastPos.y)
         this.context.lineTo(curPos.x, curPos.y)
         this.context.stroke()
+        // this.context.restore()
 
         this.data.canvasInfor.lastPos=curPos
         this.context.draw(true)
@@ -77,9 +82,16 @@ Page({
     touchEndFun(e) {
 
     },
+    clearTapFun(){
+        let windowWidth=this.data.windowWidth,
+            windowHeight=this.data.windowHeight;
+        console.log(this.context.getActions())
+        this.context.clearRect(0,0,windowWidth,windowHeight); 
+        this.context.draw(true)
+    },
     // 画最外层边框+米字格
     renderOuterBorderFun() {
-        const context = wx.createCanvasContext('canvas');
+        const context = wx.createCanvasContext('canvasBg');
         const canvasWidth = this.data.windowWidth,
               canvasHeight = canvasWidth;
         const marginSize = 10;
--- pages/other/other.js
+++ pages/other/other.js
@@ -1,6 +1,63 @@
 var app = getApp()
 Page({
     data: {
+    	selectImageSrc:''
+    },
+    onReady(){
+		
+    },
+    // 选择图片
+    chooseImage(){
+		wx.chooseImage({
+			success:(res)=>{
+				this.setData({
+					selectImageSrc:res.tempFilePaths[0]
+				})
+				
+				wx.request({
+					url:'https://www.tahappy.com/getImgPath',
+					data:{
+						urlData:res.tempFilePaths[0]
+					},
+					method:'GET',
+					success:function(res){
+						console.log(res)
+					}
+				})
+			}
+		})
+    },
+    uploadImage(){
+    // 	wx.request({
+    //         url: 'https://www.tahappy.com/getfile',
+    //         method: 'POST',
+    //         success(res){
+				// console.log(res)
+    //         }
+    // 	})
+
+    var uploadImage = this.data.selectImageSrc
+     console.log(uploadImage);
+	var reader = new FileReader()
+     console.log(reader);
+
+	reader.onload = function (e) {
+	      var arrayBuffer = reader.result;
+	      var base64 = wx.arrayBufferToBase64(arrayBuffer)
+	      console.log(base64);
+	}
+	// reader.readAsArrayBuffer(new Blob(this.data.imageList))
+	
+    	wx.uploadFile({
+			url: 'https://www.tahappy.com/getfile',
+			filePath: this.data.selectImageSrc,
+			name: 'file',
+			formData:{
+		        'user': 'test'
+		    },
+		    success: function(res){
+		        var data = res.data
+		    }
+		})
     }
-    
 })
